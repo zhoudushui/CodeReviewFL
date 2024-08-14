8e7190f0ca4cf39b21938b4e86fa994bd9430967
wujunjie123690 <2995290576@qq.com>
2024-08-14 16:39:34 +0800

    Add mnist_iid_notuniform

--- utils/sampling.py
+++ utils/sampling.py
@@ -21,6 +21,33 @@ def mnist_iid(dataset, num_users):
         all_idxs = list(set(all_idxs) - dict_users[i])
     return dict_users
 
+def mnist_iid_notuniform(dataset, num_users):
+    #创建一种分配数量不同但依然满足独立同分布的数据分配方法
+    min_samples = int(len(dataset) / num_users) - 150
+    max_samples = int(len(dataset) / num_users) + 150
+    num_items = np.random.randint(min_samples, max_samples, size=num_users)
+    total_samples = sum(num_items)
+    all_idxs = [i for i in range(len(dataset))]
+
+    # Shuffle the indices to ensure randomness
+    np.random.shuffle(all_idxs)
+
+    # Ensure that the total_samples matches the original dataset size
+    if total_samples > len(dataset):
+        raise ValueError("Total samples requested exceeds the size of the dataset.")
+
+    dict_users = {}
+    start_idx = 0
+    for i in range(num_users):
+        end_idx = start_idx + num_items[i]
+        dict_users[i] = set(all_idxs[start_idx:end_idx])
+        start_idx = end_idx
+
+    # Ensure all indices are assigned
+    assert start_idx == total_samples, "Not all indices have been assigned."
+
+    return dict_users
+
 def mnist_noniid(dataset, num_users):
     """
     Sample non-I.I.D client data from MNIST dataset
